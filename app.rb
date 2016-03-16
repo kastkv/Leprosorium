24d66d1a2d8261e5bb17e47e098dfea313affb81
KV <kv@iMac-KV.local>
2016-03-16 14:54:29 -0700

    Add stub for /new

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' # чтобы не перезапускать sinatra каждый раз
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/New' do   #добавляем обработчик
+ erb "Hello World" # чтобы выводился с нашим шаблоном пишем erb
 end

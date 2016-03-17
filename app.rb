096f18f4d6dd272a1a3cec442655e214c5627983
KV <kv@iMac-KV.local>
2016-03-16 22:04:03 -0700

    Add stub for /new

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
-get '/New' do   #добавляем обработчик
- erb "Hello World" # чтобы выводился с нашим шаблоном пишем erb
+# get '/New' do   #добавляем обработчик
+#  erb "Hello World" # чтобы выводился с нашим шаблоном пишем erb
+# end
+
+get '/New' do   
+ erb :new
 end

b9e9590c7790804488a209a9af77a570a9ddf2a3
KV <kv@iMac-KV.local>
2016-03-17 12:39:00 -0700

    Read content variable from post request

--- app.rb
+++ app.rb
@@ -14,3 +14,9 @@ end
 get '/New' do   
  erb :new
 end
+
+post '/new' do #отправляем post запрос
+	content = params[:content] # переменной content присваем значение textarea(отправляем ее), обращаемся к переменно по имени name="content"
+
+	erb "You typed #{content}" #выводим, сто мы ввели(т.е. мы обращаемся к конкретной переменной)
+end	
